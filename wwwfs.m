ecbff06d947cf3f7bafd4bb6f7f47a45e9c08580
Ruiling Zhang <zero00094@outlook.com>
2016-06-05 02:26:34 +0200

    Update wwwfs.m
    set the third node be destroyed and choose the first 8 nodes which still can work as the node mask

--- wwwfs.m
+++ wwwfs.m
@@ -1,9 +1,10 @@
 clear;close;clc;
 % numBytes = input('numBytes = ');
- N = input('N (N must be 1, 2, 4 or 8) = \n');
+% N = input('N (N must be 1, 2, 4 or 8) = \n');
 % numBytes = 10;
-% N = 8;
-t = 2;
+N = 8;
+% t = 2;
+t = input('t = ');
 numNodes = 2^t+N;
 %read an image as original data
 imdata = imread('test2.jpg');	
@@ -43,16 +44,33 @@ for blockIndex = 1:numBlocks
     STORAGE(:,blockIndex) =  block*G;
 end
 x=uint8(STORAGE.x);
-for i=1:numNodes
-    FileName=['block',num2str(i),'.txt'];
-    fid=fopen(FileName,'w');
-    fprintf(fid,'%d',x(i,:));
-    fclose(fid); 
-end
+% for i=1:numNodes
+%     FileName=['block',num2str(i),'.txt'];
+%     fid=fopen(FileName,'w');
+%     fprintf(fid,'%d',x(i,:));
+%     fclose(fid); 
+% end
+
 % Decode data using a subset of the nodes.
 % The data can be decoded using any N nodes.
-nodeMask = input('choose 8 nodes for decoding (use [] input style) = \n'); % Select what N nodes to use when decoding
-% nodeMask = 1:N;
+% nodeMask = input('choose 8 nodes for decoding (use [] input style) = \n'); % Select what N nodes to use when decoding
+nodeMask = 1:numNodes;   % first choose all of the node as mask
+noteMask_bit = toBitmask(nodeMask);
+noteMask_bit(3) = 0;    %set the third node can not use
+num = 0;
+% choose the first 8 nodes which still can use as the node mask
+for i = 1:numNodes
+        nodeMask_choose(i)=noteMask_bit(i);
+    if noteMask_bit(i)==1
+        num = num+1;
+    end
+    if num == 8
+        break
+    end
+end
+%recover the chosen bit mask back to vector mask
+nodeMask_recover = toIndexVector(nodeMask_choose);
+
 % Allocate space for the decoded data
 decDATA = zeros(size(DATA));
 decDATA = gf(decDATA,N);
@@ -60,8 +78,8 @@ decDATA = gf(decDATA,N);
 % Iterate over the encoded blocks and solve a system of linear equations
 % for every block.
 for blockIndex = 1:numBlocks
-    block = STORAGE(nodeMask,blockIndex)';
-    decDATA(:,blockIndex) = block/G(:,nodeMask);
+    block = STORAGE(nodeMask_recover,blockIndex)';
+    decDATA(:,blockIndex) = block/G(:,nodeMask_recover);
 end
 
 % Compare decoded data with the original data
@@ -69,10 +87,10 @@ end
 decDATA = reshape(decDATA,1,N*numBlocks);
 decBYTES = byteNarray(decDATA,N);
 data_recover=reshape(decBYTES,imsize(1),imsize(2),imsize(3));
-figure,imshow(data_recover);
+figure,imshow(data_recover);             % show recover file
 if sum(abs(BYTES - decBYTES)) ~= 0
-   error('Failure')
+   disp('Failure')
+else
+    disp('Success!')
 end
-disp('Success!')
-
 
